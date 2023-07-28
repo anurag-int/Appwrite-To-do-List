11f2717038e942c68bcb11bc75c0830af80b80f1
anurag-int <ag6526803@gmail.com>
2023-07-28 18:17:49 +0530

    Added all components

--- my-todo-list/src/appwrite/appwrite.js
+++ my-todo-list/src/appwrite/appwrite.js
@@ -6,11 +6,11 @@ const client = new Client();
 client.setEndpoint("http://localhost/v1").setProject("64c3798b561bebdaec67");
 
 
+
 // Account to check the valid user
-const account = new Account(client);
+export const account = new Account(client);
 
 
 //database pass(client, database-id)
-export const database = new Databases(client, "64c379e83a01e4e7c325");
+export const databases = new Databases(client, "64c379e83a01e4e7c325");
 
-export default account;
